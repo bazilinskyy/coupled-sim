fc655ad6058baa77c925e37a393a52ad5c1be429
Kirsten+Marloes <57136848+BEPcoupledsimulator@users.noreply.github.com>
2019-11-14 14:49:36 +0100

    Adjusted Scenario5
    Added car spawners and traffic circuits for Scenario 5. Raised zebra crossing. Added a lot of car prefabs. :)

---
+++ Assets/SyncedCarSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Utility;
+
+//example implementation of computer controlled car spawning logic 
+public class SyncedCarSpawner : CarSpawnerBase
+{
+    [SerializeField]
+    protected Transform SpawnPoint;
+    [SerializeField]
+    protected WaypointCircuit Track;
+    public AICar AutonomousVehicle;
+    protected override IEnumerator SpawnCoroutine()
+    {
+        Spawn(AutonomousVehicle, SpawnPoint.position, SpawnPoint.rotation, Track, false);
+        yield return new WaitForSeconds(5);
+        //yield return new WaitForSeconds(5);
+        //Spawn(AutonomousVehicle, SpawnPoint.position, SpawnPoint.rotation, Track, false);
+        //yield return new WaitForSeconds(5);
+        //Spawn(AutonomousVehicle, SpawnPoint.position, SpawnPoint.rotation, Track, true);
+    }
+}
