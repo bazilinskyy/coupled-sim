24a8221e70e6354caec84db1d1b88a55016e89f8
Johnson-Mok <55184234+Johnson-Mok@users.noreply.github.com>
2020-05-08 14:15:21 +0200

    AICar script stop for 2 seconds
    Functionalized the AICar script.
    Added complete halt BrakeZ and BrakeX.
    Created the standard experimentDefinition to work on from now on.

--- Assets/Scripts/Adapted/AICar.cs
+++ Assets/Scripts/Adapted/AICar.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 using UnityStandardAssets.Utility;
 
 // This script was adapted from the study of De Clercq.
+// This script is further modified by Johnson Mok
 
 public class AICar : MonoBehaviour
 {
@@ -33,10 +34,12 @@ public class AICar : MonoBehaviour
     private float startlocation;
     private float delta_distance;
 
-    public bool WaitInputX = false;
+    public bool WaitInputX = false; // deletee later
     public bool WaitTrialX = false;
     public bool WaitTrialZ = false;
-    
+    public bool BrakeX = false;
+    public bool BrakeZ = false;
+
     private GameObject ManualCarTrigger;
     private bool InitiateAV;
 
@@ -83,174 +86,18 @@ public class AICar : MonoBehaviour
         // This statement is applied when the car is just driving.
         if ((braking == false) && (reset == false))
         {
-            
-            if (jerk != 0)
-            {
-                acceleration = set_acceleration;
-                t += Mathf.Abs(jerk) / Mathf.Abs(set_acceleration) * Time.fixedDeltaTime;
-                pitch = acceleration / 3 * HasPitch;
-                this.transform.Rotate(-pitch, 0, 0);
-            }
-
-            if (set_acceleration > 0f && set_speed > speed || set_acceleration < 0f && set_speed < speed)
-            {
-                speed = speed + set_acceleration * Time.fixedDeltaTime * conversion;
-                pitch = acceleration / 3 * HasPitch;
-                this.transform.Rotate(-pitch, 0, 0);
-            }
-
-            if (acceleration != 0 && Mathf.Abs(speed) < Mathf.Abs(set_speed) * (1 + tolerance) + tolerance * 10 && Mathf.Abs(speed) > Mathf.Abs(set_speed) * (1 - tolerance) - tolerance * 10)
-            {
-                jerk = 0;
-                speed = set_speed;
-            }
-
-            theRigidbody.velocity = rotationAxis.forward * speed / conversion; // Application of calculated velocity to Rigidbody
-            
+            Car_Driving_Behaviour();
         }
 
         // This statement is applied when the car starts braking
         else if ((braking == true) && (reset == false))
         {
-            // Compute delta distance for deceleration
-            if (WaitInputX == true)
-            {
-                delta_distance = Mathf.Abs(this.gameObject.transform.position.x - triggerlocation);
-            }
-
-            else if (WaitTrialZ == true)
-            {
-                delta_distance = Mathf.Abs(this.gameObject.transform.position.z - triggerlocation);
-            }
-
-            else if (WaitTrialX == true)
-            {
-                delta_distance = Mathf.Abs(this.gameObject.transform.position.x - triggerlocation);
-            }
-            // Apply delta_distance for deceleration 
-            // Formula: v = sqrt(u^2 + 2*a*s) with v = final velocity; u = initial velocity; a = acceleration; s = distance covered. 
-            speed = Mathf.Sqrt(900 + 2 * set_acceleration * Mathf.Pow(conversion, 2) * delta_distance); // Application of conversion of km/h to m/s which needs to be squared
-
-            // Slowing down            
-            // Compute pitch for deceleration
-            if (speed > 10f && delta_distance < 17f) // When speed larger than 10 km/h, pitch increases to 0.5 degrees
-            {
-                Timer1 += Time.deltaTime;
-                pitch = Timer1 * 6;
-                // Pitches larger than 0.5 degrees are capped at 0.5 degrees.
-                if (pitch < 0.5f)
-                {
-                    this.transform.Rotate(pitch, 0, 0);
-                }
-                else
-                {
-                    this.transform.Rotate(0.5f, 0, 0);
-                }
-            }
-
-            else if (speed < 10f && delta_distance < 17f) // When speed smaller than 10 km/h, pitch slowely decreases from 0.5 degrees
-            {
-                Timer2 += Time.deltaTime;
-                pitch = 0.5f - (Timer2);
-
-                if (pitch >= 0)
-                {
-                    this.transform.Rotate(pitch, 0, 0);
-                }
-
-            }
-
-            else if (Double.IsNaN(speed) || speed > 0f && speed < 5f && delta_distance > 17f) // Once distance is far enough and speed is almost zero, decrease pitch even more
-            {
-                Timer2 += Time.deltaTime;
-                pitch = 0.5f - (Timer2);
-
-                if (pitch >= 0)
-                {
-                    this.transform.Rotate(pitch, 0, 0);
-                }
-
-                speed = 0f;
-                theRigidbody.velocity = new Vector3(0,0,0); // Apply zero velocity 
-            }
-
-            if (speed <= 0 && delta_distance > 16f)  // If car is standing still, change pitch back to zero.
-            {
-                Timer2 += Time.deltaTime;
-                pitch = 0.5f - (Timer2);
-
-                if (pitch >= 0) // Apply pitch only when larger than 0 degrees.
-                {
-                    this.transform.Rotate(pitch, 0, 0);
-                }
-                Debug.Log(Timer2);
-
-                if (Timer2 >= 2f) // After standing still for two seconds, passenger can initiate driving again by pressing space.
-                {
-                    if (WaitInputX == true && Input.GetKeyDown(KeyCode.Space))
-                    {
-                        braking = false;
-                        reset = true;
-                        set_acceleration = 1f;
-                        set_speed = 30;
-                        startlocation = this.gameObject.transform.position.x - 0.10f;
-                    }
-                    
-                    else if (WaitTrialZ == true && InitiateAV == true) //Input.GetKeyDown(KeyCode.Space))
-                    {
-                        braking = false;
-                        reset = true;
-                        set_acceleration = 1f;
-                        set_speed = 30;
-                        startlocation = this.gameObject.transform.position.z - 0.10f;
-                    }
-                    else if (WaitTrialX == true && InitiateAV == true) //Input.GetKeyDown(KeyCode.Space))
-                    {
-                        braking = false;
-                        reset = true;
-                        set_acceleration = 1f;
-                        set_speed = 30;
-                        startlocation = this.gameObject.transform.position.x - 0.10f;
-                    }
-                }
-            }
-            theRigidbody.velocity = rotationAxis.forward * speed / conversion; // Application of calculated velocity to Rigidbody 
+            Decelerate_Car();
         }
         // This statement is applied when the car stood still and is resetting its speed.
         else if ((braking == false) && (reset == true))
         {
-            // Accelerating in the X direction
-            if (WaitInputX == true)
-            {
-                delta_distance = Mathf.Abs(this.gameObject.transform.position.x - startlocation);
-            }
-
-            // Accelerating in the Z direction
-            else if (WaitTrialZ == true)
-            {
-                delta_distance = Mathf.Abs(this.gameObject.transform.position.z - startlocation);
-            }
-
-            // Accelerating in the X direction
-            else if (WaitTrialX == true)
-            {
-                delta_distance = Mathf.Abs(this.gameObject.transform.position.x - startlocation);
-            }
-
-            speed = Mathf.Sqrt(2 * set_acceleration * Mathf.Pow(conversion, 2) * delta_distance);
-            if (speed < 2f)
-            {
-                speed = 2f;
-            }
-            theRigidbody.velocity = rotationAxis.forward * speed / conversion; // Application of calculated velocity to Rigidbody 
-
-            if (speed >= 10f)
-            {
-                reset = false;
-                WaitInputX = false;
-                WaitTrialZ = false;
-                WaitTrialX = false;
-            }
+            Reset_Speed_After_Stopping();
         }
     }
 
@@ -284,37 +131,212 @@ public class AICar : MonoBehaviour
                 Destroy(target.gameObject);
             }
         }
-
-        // Change of tag here that causes deceleration when hitting trigger in X direction
-        else if (other.gameObject.CompareTag("WaitInput_X"))
+        // Change of tag here that causes deceleration when hitting a trigger.
+        else
         {
-            triggerlocation = other.gameObject.transform.position.x;
-            braking = true;
-            WaitInputX = true;
-            set_speed = other.GetComponent<SpeedSettings>().speed;
-            set_acceleration = other.GetComponent<SpeedSettings>().acceleration;
-            jerk = -Mathf.Abs(other.GetComponent<SpeedSettings>().jerk);             
+            Brake_AV(other);
         }
-        // Change of tag here that causes deceleration when hitting trigger in Z direction
-        else if (other.gameObject.CompareTag("StartTrial_Z"))
+    }
+
+    /////////////////////////////   LOCAL FUNCTIONS     /////////////////////////////
+    
+    void Brake_AV(Collider other)
+    {
+        if (other.gameObject.CompareTag("StartTrial_Z"))                // Change tag, resume driving after stopping for 2 seconds in the Z direction.
         {
-            triggerlocation = other.gameObject.transform.position.z; 
-            braking = true;
             WaitTrialZ = true;
-            set_speed = other.GetComponent<SpeedSettings>().speed;
-            set_acceleration = other.GetComponent<SpeedSettings>().acceleration;
-            jerk = -Mathf.Abs(other.GetComponent<SpeedSettings>().jerk);
+            triggerlocation = other.gameObject.transform.position.z;
+        }
+        else if (other.gameObject.CompareTag("Brake_Z"))                // Change tage, stop completely.
+        {
+            BrakeZ = true;
+            triggerlocation = other.gameObject.transform.position.z;
+        }
+        else if (other.gameObject.CompareTag("StartTrial_X"))           // Change tag, resume driving after stopping for 2 seconds in the X direction.
+        {
+            WaitTrialX = true;
+            triggerlocation = other.gameObject.transform.position.x;
+        }
+        else if (other.gameObject.CompareTag("Brake_X"))                // Change tage, stop completely.
+        {
+            BrakeX = true;
+            triggerlocation = other.gameObject.transform.position.x;
+        }
+        Brake_Set_Variables(other);
+    }
+
+    void Brake_Set_Variables(Collider other)
+    {
+        braking = true;
+        set_speed = other.GetComponent<SpeedSettings>().speed;
+        set_acceleration = other.GetComponent<SpeedSettings>().acceleration;
+        jerk = -Mathf.Abs(other.GetComponent<SpeedSettings>().jerk);
+    }
+
+    // Function to set car behaviour during driving
+    void Car_Driving_Behaviour()
+    {
+        if (jerk != 0)
+        {
+            acceleration = set_acceleration;
+            t += Mathf.Abs(jerk) / Mathf.Abs(set_acceleration) * Time.fixedDeltaTime;
+            pitch = acceleration / 3 * HasPitch;
+            this.transform.Rotate(-pitch, 0, 0);
         }
 
-        // Change of tag here that causes deceleration when hitting trigger in Z direction
-        else if (other.gameObject.CompareTag("StartTrial_X"))
+        if (set_acceleration > 0f && set_speed > speed || set_acceleration < 0f && set_speed < speed)
         {
-            triggerlocation = other.gameObject.transform.position.x;
-            braking = true;
-            WaitTrialX = true;
-            set_speed = other.GetComponent<SpeedSettings>().speed;
-            set_acceleration = other.GetComponent<SpeedSettings>().acceleration;
-            jerk = -Mathf.Abs(other.GetComponent<SpeedSettings>().jerk);
+            speed = speed + set_acceleration * Time.fixedDeltaTime * conversion;
+            pitch = acceleration / 3 * HasPitch;
+            this.transform.Rotate(-pitch, 0, 0);
+        }
+
+        if (acceleration != 0 && Mathf.Abs(speed) < Mathf.Abs(set_speed) * (1 + tolerance) + tolerance * 10 && Mathf.Abs(speed) > Mathf.Abs(set_speed) * (1 - tolerance) - tolerance * 10)
+        {
+            jerk = 0;
+            speed = set_speed;
+        }
+
+        theRigidbody.velocity = rotationAxis.forward * speed / conversion; // Application of calculated velocity to Rigidbody
+    }
+
+    void Compute_Pitch_Deceleration()
+    {
+        // When speed larger than 10 km/h, pitch increases to 0.5 degrees
+        if (speed > 10f && delta_distance < 17f) 
+        {
+            Timer1 += Time.deltaTime;
+            pitch = Timer1 * 6;
+            
+            if (pitch < 0.5f)                           // Pitches larger than 0.5 degrees are capped at 0.5 degrees.
+            {
+                this.transform.Rotate(pitch, 0, 0);
+            }
+            else
+            {
+                this.transform.Rotate(0.5f, 0, 0);
+            }
+        }
+
+        // When speed smaller than 10 km/h, pitch slowely decreases from 0.5 degrees
+        else if (speed < 10f && delta_distance < 17f) 
+        {
+            Timer2 += Time.deltaTime;
+            pitch = 0.5f - (Timer2);
+
+            if (pitch >= 0)
+            {
+                this.transform.Rotate(pitch, 0, 0);
+            }
+
+        }
+
+        // Once distance is far enough and speed is almost zero, decrease pitch even more
+        else if (Double.IsNaN(speed) || speed > 0f && speed < 5f && delta_distance > 17f) 
+        {
+            Timer2 += Time.deltaTime;
+            pitch = 0.5f - (Timer2);
+
+            if (pitch >= 0)
+            {
+                this.transform.Rotate(pitch, 0, 0);
+            }
+
+            speed = 0f;
+            theRigidbody.velocity = new Vector3(0, 0, 0);   // Apply zero velocity 
+        }
+
+        // If car is standing still, change pitch back to zero.
+        if (speed <= 0 && delta_distance > 16f)  
+        {
+            Timer2 += Time.deltaTime;
+            pitch = 0.5f - (Timer2);
+
+            if (pitch >= 0)                                 // Apply pitch only when larger than 0 degrees.
+            {
+                this.transform.Rotate(pitch, 0, 0);
+            }
+            Debug.Log(Timer2);
+
+            // After standing still for two seconds, passenger can initiate driving again by pressing space.
+            Resume_Driving_After_Stop(2f);
+        }
+    }
+
+    // Function to resume driving after stopping for 'breaktime' seconds.
+    void Resume_Driving_After_Stop(float breaktime)
+    {
+        if (Timer2 >= breaktime)
+        {
+            if (WaitTrialZ == true) 
+            {
+                braking = false;
+                reset = true;
+                set_acceleration = 1f;
+                set_speed = 30;
+                startlocation = this.gameObject.transform.position.z - 0.10f;
+            }
+            else if (WaitTrialX == true)
+            {
+                braking = false;
+                reset = true;
+                set_acceleration = 1f;
+                set_speed = 30;
+                startlocation = this.gameObject.transform.position.x - 0.10f;
+            }
+        }
+    }
+
+    // Function to decelerate car when braking
+    void Decelerate_Car()
+    {
+        // Compute delta distance for deceleration
+        if (WaitTrialX == true || BrakeX == true)
+        {
+            delta_distance = Mathf.Abs(this.gameObject.transform.position.x - triggerlocation);
+        }
+
+        else if (WaitTrialZ == true || BrakeZ == true)
+        {
+            delta_distance = Mathf.Abs(this.gameObject.transform.position.z - triggerlocation);
+        }
+
+        // Apply delta_distance for deceleration 
+        // Formula: v = sqrt(u^2 + 2*a*s) with v = final velocity; u = initial velocity; a = acceleration; s = distance covered. 
+        speed = Mathf.Sqrt(900 + 2 * set_acceleration * Mathf.Pow(conversion, 2) * delta_distance); // Application of conversion of km/h to m/s which needs to be squared
+
+        // Slowing down            
+        // Compute pitch for deceleration
+        Compute_Pitch_Deceleration();
+        theRigidbody.velocity = rotationAxis.forward * speed / conversion; // Application of calculated velocity to Rigidbody 
+    }
+
+    void Reset_Speed_After_Stopping()
+    {
+        // Accelerating in the Z direction
+        if (WaitTrialZ == true)
+        {
+            delta_distance = Mathf.Abs(this.gameObject.transform.position.z - startlocation);
+        }
+
+        // Accelerating in the X direction
+        else if (WaitTrialX == true)
+        {
+            delta_distance = Mathf.Abs(this.gameObject.transform.position.x - startlocation);
+        }
+
+        speed = Mathf.Sqrt(2 * set_acceleration * Mathf.Pow(conversion, 2) * delta_distance);
+        if (speed < 2f)
+        {
+            speed = 2f;
+        }
+        theRigidbody.velocity = rotationAxis.forward * speed / conversion; // Application of calculated velocity to Rigidbody 
+
+        if (speed >= 10f)
+        {
+            reset = false;
+            WaitTrialZ = false;
+            WaitTrialX = false;
         }
     }
 }
