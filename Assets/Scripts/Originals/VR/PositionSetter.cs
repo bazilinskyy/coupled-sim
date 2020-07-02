58de1adeea1420406ce812e894a88799ac38275b
Johnson-Mok <55184234+Johnson-Mok@users.noreply.github.com>
2020-07-02 14:11:49 +0200

    Fixing the Varjo camera placement on prefabs
    - To Solve: Sometimes the camera changes position due to vehicle stutter.
    - Need to see if the problem remains with a smooth vehicle track.

--- Assets/Scripts/Originals/VR/PositionSetter.cs
+++ Assets/Scripts/Originals/VR/PositionSetter.cs
@@ -1,10 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Varjo;
+
 [ExecuteInEditMode]
 public class PositionSetter : MonoBehaviour {
     public Transform target;
+    private Vector3 hmdPosition;
+    private Vector3 changePos;
+    private Vector3 nextPosition;
+    private float moveSpeed = 1;
 
+    public bool Use_HMD;
 	void LateUpdate () {
-        transform.position = target.position; //  + new Vector3(-0.03f,+0.16f,-0.7f);
-	}
+        if (Use_HMD == true)
+        {
+            // Position setting
+            hmdPosition = VarjoManager.Instance.GetHMDPosition(VarjoPlugin.PoseType.CENTER);
+            changePos = target.position;
+            if (hmdPosition.y > 0.0f)
+            {
+                changePos.y = changePos.y - hmdPosition.y;
+            }
+            transform.position = changePos;
+
+            // Smoothening
+            //transform.position = Vector3.Lerp(changePos, nextPosition, Time.deltaTime * moveSpeed);
+        }
+        else if(Use_HMD == false)
+        {
+            transform.position = target.position; //  + new Vector3(-0.03f,+0.16f,-0.7f);
+        }
+
+        Debug.LogError($"Target pos = {target.position}");
+        Debug.LogError($"HMD pos = {hmdPosition}");
+        //Debug.LogError($"change pos = {changePos}");
+        Debug.LogError($"transform position {gameObject.transform.position}");
+        //Debug.LogError($"transform local pos = {transform.localPosition}");
+    }
 }
