7c36f6388972c495afe2643061b569f5e626b1b5
Johnson-Mok <55184234+Johnson-Mok@users.noreply.github.com>
2020-07-03 16:45:27 +0200

    Update PositionSetter.cs
    - works in all directions without presetting rotation.
    - To solve: position setting during turns

--- Assets/Scripts/Originals/VR/PositionSetter.cs
+++ Assets/Scripts/Originals/VR/PositionSetter.cs
@@ -2,43 +2,41 @@
 using System.Collections;
 using Varjo;
 
-// In the coupled-sim it functions in the y-direction, but not in the x- and z-direction.
-// In the varjo testing scene it functions in all the directions.
+// To Solve: position setting during turns.
+public enum enumRot
+{
+    Zero,
+    NineZero,
+    OneEightZero,
+    TwoSevenZero,
+}
 
 [ExecuteInEditMode]
 public class PositionSetter : MonoBehaviour {
     public Transform target;
+    public bool Use_HMD;
+
     private Vector3 hmdPosition;
     private Vector3 changePos;
     private float rot;
+    private enumRot rot_;
 
-    public bool Use_HMD;
-    private void Start()
-    {
-        rot = gameObject.transform.rotation.eulerAngles.y;
-        Debug.LogError($"Rotation = {gameObject.transform.rotation.eulerAngles.y}");
-    }
 
     void LateUpdate () {
         if (Use_HMD == true)
         {
             // Get HMD pos
             hmdPosition = VarjoManager.Instance.GetHMDPosition(VarjoPlugin.PoseType.CENTER);
-            Debug.LogError($"1 HMD pos = {hmdPosition}");
+
             // Get target pos
             changePos = target.position;
-            Debug.LogError($"2 target position = {target.position}");
+
             // Effect of HMD on target pos
-            effectHMD(rot);
-            /*changePos.x = changePos.x + hmdPosition.x; // left pos for world, left neg for hmd
-            changePos.z = changePos.z + hmdPosition.z;
-            if (hmdPosition.y > 0.0f)
-            {
-                changePos.y = changePos.y - hmdPosition.y; 
-            }*/
+            rot = gameObject.transform.rotation.eulerAngles.y;
+            determineAngle(rot);
+            effectHMD(rot_);
 
             transform.position = changePos;
-            Debug.LogError($"3 (final) transform pos = {transform.position}");
         }
         else if(Use_HMD == false)
         {
@@ -46,11 +44,31 @@ public class PositionSetter : MonoBehaviour {
         } 
     }
 
-    private void effectHMD(float rot)
+    private void determineAngle(float rot)
     {
-        switch (rot)
+        if (rot < 40 || rot > 320)          // 0 +- 40
         {
-            case 0:
+            rot_ = enumRot.Zero;
+        }
+        else if (rot > 50 && rot < 130)     // 90 +- 40
+        {
+            rot_ = enumRot.NineZero;
+        }
+        else if (rot > 140 && rot < 220)    // 180 +- 40
+        {
+            rot_ = enumRot.OneEightZero;
+        }
+        else if (rot > 230 && rot < 310)    // 270 +- 40
+        {
+            rot_ = enumRot.TwoSevenZero;
+        }
+    }
+
+    private void effectHMD(enumRot rot_)
+    {
+        switch (rot_)
+        {
+            case enumRot.Zero:
                 changePos.x = changePos.x - hmdPosition.x; 
                 changePos.z = changePos.z - hmdPosition.z;
                 if (hmdPosition.y > 0.0f)
@@ -58,7 +76,7 @@ public class PositionSetter : MonoBehaviour {
                     changePos.y = changePos.y - hmdPosition.y;
                 }
                 break;
-            case 90:
+            case enumRot.NineZero:
                 changePos.x = changePos.x - hmdPosition.z;
                 changePos.z = changePos.z + hmdPosition.x;
                 if (hmdPosition.y > 0.0f)
@@ -66,7 +84,7 @@ public class PositionSetter : MonoBehaviour {
                     changePos.y = changePos.y - hmdPosition.y;
                 }
                 break;
-            case 180:
+            case enumRot.OneEightZero:
                 changePos.x = changePos.x + hmdPosition.x;
                 changePos.z = changePos.z + hmdPosition.z;
                 if (hmdPosition.y > 0.0f)
@@ -74,7 +92,7 @@ public class PositionSetter : MonoBehaviour {
                     changePos.y = changePos.y - hmdPosition.y;
                 }
                 break;
-            case 270:
+            case enumRot.TwoSevenZero:
                 changePos.x = changePos.x + hmdPosition.z;
                 changePos.z = changePos.z - hmdPosition.x;
                 if (hmdPosition.y > 0.0f)
