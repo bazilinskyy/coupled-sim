5f6efb01b3f86a5e3c746eda0f3424b8ec1dadb9
Johnson-Mok <55184234+Johnson-Mok@users.noreply.github.com>
2020-08-14 17:05:46 +0200

    Create random list order for exp def

--- Assets/Scripts/New/SceneSelector.cs
+++ Assets/Scripts/New/SceneSelector.cs
@@ -22,6 +22,11 @@ public class SceneSelector : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        SceneRandomizer();
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "NEXT")
@@ -34,6 +39,7 @@ public class SceneSelector : MonoBehaviour
             PersistentManager.Instance.nextScene = true;
             Debug.LogError("Hit, going to the next scene");
 
+            // Select next exp
             PersistentManager.Instance.experimentnr++;
             Debug.LogError($"persistent experiment nr = {PersistentManager.Instance.experimentnr}");
         }
@@ -85,5 +91,66 @@ public class SceneSelector : MonoBehaviour
             PersistentManager.Instance.useEyeTracking = true;
         }
         Debug.LogError($"exp nr = {PersistentManager.Instance.experimentnr}; 1if = {PersistentManager.Instance.experimentnr < 4}; 2if = {PersistentManager.Instance.experimentnr >= 4}; tracking = {PersistentManager.Instance.useEyeTracking} ");
+    }
+
+    private void SceneRandomizer()
+    {
+        // Randomize exp 0-3, every exp 3 times
+        List<int> Block_one = makeList(0, 3, 3);
+        Block_one = Shuffler(Block_one);
+
+        // Randomize Mapping 1, exp 4-7
+        List<int> Block_two = makeList(4, 7, 3);
+        Block_two = Shuffler(Block_two);
+
+        // Randomize Mapping 2, exp 8-11
+        List<int> Block_three = makeList(8, 11, 3);
+        Block_three = Shuffler(Block_three);
+
+        // Randomize choosing mapping 1 or 2
+        int randomInt = Random.Range(1, 2);
+
+        // Set order of the blocks
+        if(randomInt == 1)
+        {
+            Block_one.AddRange(Block_two);
+            Block_one.AddRange(Block_three);
+        }
+        else if (randomInt == 2)
+        {
+            Block_one.AddRange(Block_three);
+            Block_one.AddRange(Block_two);
+        }
+
+        Debug.LogError($"Randomint = {randomInt}");
+        for (int i = 0; i < Block_one.Count; i++)
+        {
+            Debug.LogError($"List one = {Block_one[i]}");
+        }
+    }
+
+    private List<int> Shuffler(List<int> _Block)
+    {
+        for (int i = 0; i < _Block.Count; i++)
+        {
+            int temp = _Block[i];
+            int randomIndex = Random.Range(i, _Block.Count);
+            _Block[i] = _Block[randomIndex];
+            _Block[randomIndex] = temp;
+        }
+        return _Block;
+    }
+
+    private List<int> makeList(int start, int end, int reps)
+    {
+        List<int> Block = new List<int>();
+        for (int i = start; i <= end; i++)
+        {
+            for (int j = 0; j <= reps-1; j++)
+            {
+                Block.Add(i);
+            }
+        }
+        return Block;
     }
 }
