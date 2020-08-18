9299ee887c61693e4b7529aae90b85ae9bf0cad6
Johnson-Mok <55184234+Johnson-Mok@users.noreply.github.com>
2020-08-18 16:35:08 +0200

    Update Logreader.m

--- Matlab/Logreader.m
+++ Matlab/Logreader.m
@@ -7,7 +7,8 @@ close all
 % filename = "LOG_N.csv";
 filename = getlatestfile("Logdata");
 filename = "Logdata/" + filename;
-Data = loadData(filename);
+% Data = loadData(filename);
+Data = loadData("Logdata/a1.csv");
 
 % Variables to use
 distance = Data.distance;
@@ -22,42 +23,51 @@ T = getTime(distance, dt, 1); % Set last argument to 1 to display table.
 
 %% Graphs
 figure;
+grid on;
+title("Distances");
 yyaxis left
 plot(distance);
+ylim([-10 50]);
 ylabel("eye gaze distance");
 yyaxis right
 plot(pos_z);
+ylim([-80 100]);
 ylabel("AV pos_z");
 
-figure;
-subplot(3,1,1);
-plot(world_origin.x);
-ylabel("world origin x");
-subplot(3,1,2);
-plot(world_origin.y);
-ylabel("world origin y");
-subplot(3,1,3);
-plot(world_origin.z);
-ylabel("world origin z");
+% figure;
+% subplot(3,1,1);
+% plot(world_origin.x);
+% ylabel("world origin x");
+% title("World origin");
+% subplot(3,1,2);
+% plot(world_origin.y);
+% ylabel("world origin y");
+% subplot(3,1,3);
+% plot(world_origin.z);
+% ylabel("world origin z");
+% 
+% 
+% figure;
+% subplot(3,1,1);
+% plot(hmd_origin.x);
+% ylabel("hmd origin x");
+% title("HMD origin");
+% subplot(3,1,2);
+% plot(hmd_origin.y);
+% ylabel("hmd origin y");
+% subplot(3,1,3);
+% plot(hmd_origin.z);
+% ylabel("hmd origin z");
+% 
+% figure;
+% subplot(3,1,1);
+% plot(world_dir.x);
+% ylabel("world dir x");
+% title("world dir");
+% subplot(3,1,2);
+% plot(world_dir.y);
+% ylabel("world dir y");
+% subplot(3,1,3);
+% plot(world_dir.z);
+% ylabel("world dir z");
 
-figure;
-subplot(3,1,1);
-plot(hmd_origin.x);
-ylabel("hmd origin x");
-subplot(3,1,2);
-plot(hmd_origin.y);
-ylabel("hmd origin y");
-subplot(3,1,3);
-plot(hmd_origin.z);
-ylabel("hmd origin z");
-
-figure;
-subplot(3,1,1);
-plot(world_dir.x);
-ylabel("hmd origin x");
-subplot(3,1,2);
-plot(hmd_origin.y);
-ylabel("hmd origin y");
-subplot(3,1,3);
-plot(hmd_origin.z);
-ylabel("hmd origin z");
