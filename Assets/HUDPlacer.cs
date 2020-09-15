d0ccf9754012e15a8723e2e2b6e97707ab8b6fc3
MarcSchotman <m_a_schotman@hotmail.com>
2020-09-15 11:35:16 +0200

    Add visual angle and distance property to HUD

---
+++ Assets/HUDPlacer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDPlacer : MonoBehaviour
+{
+    public float visualAngleVertical; 
+    public float visualAngleHorizontal;
+    public float distance;
+    public Transform headPosition;
+
+    public bool _pressMeToPlace; private bool pressMeToPlace;
+    private void Start()
+    {
+        PlaceHUD();
+    }
+    void CheckForChanges() {
+        if (pressMeToPlace != _pressMeToPlace)
+        {
+            pressMeToPlace = _pressMeToPlace;
+            PlaceHUD();
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        CheckForChanges();
+    }
+    void PlaceHUD()
+    {
+        float visualAngleVerticalRadians = visualAngleVertical * Mathf.PI / 180;
+        float visualAngleHorizontalRadians = visualAngleHorizontal * Mathf.PI / 180;
+        Vector3 forwardVector = new Vector3(0, Mathf.Tan(visualAngleVerticalRadians) * distance, distance);
+        Vector3 sideVector = new Vector3(Mathf.Tan(visualAngleHorizontalRadians), 0, 0);
+        transform.position = headPosition.position + forwardVector + sideVector;
+    }
+}
