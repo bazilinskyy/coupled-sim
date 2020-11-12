99fc6647a377d9d3346caea12cb6996b0a597ffb
Piotr Sienkowski <piotr@lowpolydreams.com>
2020-11-12 18:47:55 +0100

    car floating problem fixed

--- Assets/Scripts/Originals/Cars/WheelRotator.cs
+++ Assets/Scripts/Originals/Cars/WheelRotator.cs
@@ -13,24 +13,6 @@ public class WheelRotator : MonoBehaviour {
 
     private float RotationSpeed;  
     private float speed = 0;
-
-    //Contact condition of wheels through fixing their y
-    private float InitialY_FL;
-    private float InitialY_FR;
-    private float InitialY_RL;
-    private float InitialY_RR;
-
-    private AICar AICarRef;
-
-    // Use this for initialization
-    void Start () {
-        InitialY_FL = FrontLeft.position.y;
-        InitialY_FR = FrontRight.position.y;
-        InitialY_RL = RearLeft.position.y;
-        InitialY_RR = RearRight.position.y;
-        //AICarRef = GetComponent<AICar>();
-        AICarRef = GetComponentInParent<AICar>();
-    }
 	
 	void Update () {
         
@@ -43,15 +25,11 @@ public class WheelRotator : MonoBehaviour {
 
         //Front Left
         FrontLeft.Rotate(RotationSpeed * Time.deltaTime, 0, 0);
-        FrontLeft.position = new Vector3 (FrontLeft.position.x, InitialY_FL ,FrontLeft.position.z);
         //Front Right
         FrontRight.Rotate(RotationSpeed * Time.deltaTime, 0, 0);
-        FrontRight.position = new Vector3(FrontRight.position.x, InitialY_FR, FrontRight.position.z);
         //Rear Left
         RearLeft.Rotate(RotationSpeed * Time.deltaTime, 0, 0);
-        RearLeft.position = new Vector3(RearLeft.position.x, InitialY_RL, RearLeft.position.z);
         //Rear Right
         RearRight.Rotate(RotationSpeed * Time.deltaTime, 0, 0);
-        RearRight.position = new Vector3(RearRight.position.x, InitialY_RR, RearRight.position.z);
     }
 }
