e8108c473b63990af3db8a352e526f0f9787bc2c
Johnson-Mok <55184234+Johnson-Mok@users.noreply.github.com>
2020-11-25 13:03:13 +0100

    Update Network expnr
    - The data was not available in the client anymore due to previous changes

--- Assets/Scripts/Adapted/VR/ClientVisualizeGaze.cs
+++ Assets/Scripts/Adapted/VR/ClientVisualizeGaze.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class ClientVisualizeGaze : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    private GameObject go_N0;
+    private int expNr;
+    private bool enter = true;
     // Update is called once per frame
     void Update()
     {
-        GameObject go_N3 = gameObject.transform.Find("NetworkObject_3").gameObject;
-        PersistentManager.Instance._visualizeGaze_client = VisualizeGazeClient((int)go_N3.transform.position.z);
+        //GameObject go_N3 = gameObject.transform.Find("NetworkObject_3").gameObject;
+        //PersistentManager.Instance._visualizeGaze_client = VisualizeGazeClient((int)go_N3.transform.position.z);
+        if(go_N0 == null)
+        {
+            go_N0 = GameObject.Find("NetworkObject_0");
+            Debug.LogError($"n0 = {go_N0}");
+        }
+        if(go_N0 != null && enter == true)
+        {
+            expNr = (int)go_N0.transform.position.x;
+            PersistentManager.Instance._visualizeGaze_client = VisualizeGazeClient(expNr);
+            Debug.LogError($"n0 posx = {(int)go_N0.transform.position.x}");
+            Debug.LogError($"Visualizegazeclient value = {PersistentManager.Instance._visualizeGaze_client}");
+            // For some reason the exp nr is substracted by 1 at random times. This is only a problem for expnr 4 since that is the start of the visualization.
+            // At expnr 3 the visualization is turned off.
+            if(expNr == 4)
+            {
+                enter = false;
+            }
+        }
     }
 
     public bool VisualizeGazeClient(int expnr)
---
+++ Assets/Scripts/Networking/Network_local_varjo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Network_local_varjo : MonoBehaviour
+{
+    public string hmdserial = "LHR-7863A1E8"; //LHR-7863A1E8 //LHR-85C3EF8C
+    // Define data to network for NetworkObject_0
+    public int experimentNr;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Get data for NetworkObject_0
+        experimentNr = PersistentManager.Instance.experimentnr;
+
+        // Apply the right poses according to the gameobject name
+        if (gameObject.name == "NetworkObject_0" && SteamVR.instance.hmd_SerialNumber == hmdserial)
+        {
+            transform.position = new Vector3(experimentNr, 0, 0);
+            //Debug.LogError($"0 - expnr = {experimentNr}");
+            //Debug.LogError($"0 - trans pos = {transform.position}");
+        }
+    }
+}
