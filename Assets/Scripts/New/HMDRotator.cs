8aa7d9923af83afe75fbe38add24c87253d4e56d
Johnson-Mok <55184234+Johnson-Mok@users.noreply.github.com>
2020-11-30 16:08:31 +0100

    Rotate pedestrian avatar based on hmd rotation

---
+++ Assets/Scripts/New/HMDRotator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using VarjoExample;
+
+public class HMDRotator : MonoBehaviour
+{
+    public GameObject Gaze;
+    public string hmdserial = "LHR-85C3EF8C"; //LHR-7863A1E8 //LHR-85C3EF8C
+
+    void Update()
+    {
+        if (SteamVR.instance.hmd_SerialNumber == hmdserial)
+        {
+            var script = Gaze.GetComponent<VarjoGazeRay_CS_1>();
+            transform.rotation = Quaternion.Euler(0, script.hmdrotation.y, 0);
+        }
+    }
+}
