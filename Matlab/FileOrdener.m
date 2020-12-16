c49e972830931bd3b99b30e2cb0a3a49fff29878
Johnson-Mok <55184234+Johnson-Mok@users.noreply.github.com>
2020-12-16 21:37:36 +0100

    Matlab Update
    - Orden files according to experiment defintion.
    - Update csv file to struct converter

---
+++ Matlab/FileOrdener.m
@@ -0,0 +1,48 @@
+%% File ordener.
+% This script reads the title of the file and categorizes it according to the experiment definition; 
+% Author: Johnson Mok
+% Last Updated: 16-12-2020
+clc
+clear
+disp('Start ordening data...');
+tic;
+
+%% Create folder structure
+name_mainF = 'Data_ordened';
+if(~exist(name_mainF, 'dir'))       % Create main folder
+    mkdir(name_mainF)
+end
+mainF = fullfile(pwd,name_mainF);   % Create string to main folder
+ExpDef_dir = cell(12,1);            % Create exp definition folders
+for i = 1:12
+    ExpDef = {join(['ED_',num2str(i-1)])};
+    ExpDef_dir(i,:) = fullfile(mainF, ExpDef);
+        if(~exist(ExpDef_dir{i}, 'dir'))
+            mkdir(ExpDef_dir{i})
+        end
+end
+
+%% Get csv files from location 
+%Get the directory contents
+directory = 'Logdata';
+dirc = dir(directory);
+%Filter out all the folders.
+dirc = dirc(find(~cellfun(@isdir,{dirc(:).name})));
+for i=1:length(dirc)
+   filename         = dirc(i).name;
+   foldername       = dirc(i).folder;
+   fileMove         = join([dirc(i).folder, '\', dirc(i).name]);
+   filename_split   = split(filename, '_');
+%    Participantnr = str2double(filename_split(2));
+%    Trialnr = str2double(filename_split(4));
+   ExpDefnr = str2double(filename_split(6));
+   for j=0:11
+        if(ExpDefnr==j)
+            movefile(fileMove,ExpDef_dir{j+1});
+        end
+   end
+end
+
+%%
+t = toc;
+disp(['Ordening data took ', num2str(t) ,' seconds.']);
--- Matlab/loadData.m
+++ Matlab/loadData.m
@@ -1,23 +1,23 @@
 function [Data, pa, pe, diAV, M] = loadData(filename, extracar)
 %% Load data
-% Needs an update everytime the Worldlogging scripts is modified.
+% This script turns the csv logging file into data structures.
+% Author: Johnson Mok
+% Last Updated: 16-12-2020
+
 disp('Start loading data...');
 tic;
-
 opts = delimitedTextImportOptions('Delimiter',';',...
                                   'DataLines', 10);
 Loaded_Data = readmatrix(filename,opts);
 
-% Split data of type char from type double
+%% Split data of type char from type double
 is_char = zeros(1,size(Loaded_Data,2));
 is_double = zeros(1,size(Loaded_Data,2));
-
 for i=1:size(Loaded_Data,2)
     check = class(Loaded_Data{1,i});
     is_char(1,i) = strcmp(check, 'char');
     is_double(1,i) = strcmp(check, 'double');
 end
-
 Data_char = zeros(length(Loaded_Data),length(is_char));
 for i=1:size(Loaded_Data,2)
     if is_char(i) == 1
@@ -26,112 +26,104 @@ for i=1:size(Loaded_Data,2)
         end
     end
 end
-
-% Data_double = zeros(length(Data),length(is_double));
-% n = 0;
-% for i=1:size(Data,2)
-%     if is_double(i) == 1
-%         for j=1:size(Data,1)
-%             n = n+1;
-%             Data_double(j,n) = Data{j,i};
-%         end
-%     end
-% end
-% toc
 M = Data_char;
 
-%% Data sound
-sound.press = M(:,10);
-sound.release = M(:,11);
+%% Data sound - needs to be put in the output too
+sound.press     = M(:,10);
+sound.release   = M(:,11);
 
-%% Data passenger and AV
-Data.dt = M(1,1); % in [s]
-Data.Time = M(:,1);
-pa.pos = struct3Coordinate(M(:,3:5));
-pa.rot = struct3Coordinate(M(:,6:8));
+% Data passenger and AV (3:54)
+Data.dt                     = M(1,1); % in [s]
+Data.Time                   = M(:,1);
+pa.pos                      = struct3Coordinate(M(:,3:5));
+pa.rot                      = struct3Coordinate(M(:,6:8));
+pa.distance                 = M(:,12);
+pa.other.frame              = M(:,13);
+pa.other.captureTime        = M(:,14);
+pa.other.hmdpos             = struct3Coordinate(M(:,15:17));
+pa.other.hmdrot             = struct3Coordinate(M(:,18:20));
+pa.other.leftEyePupilSize   = M(:,21);
+pa.other.rightEyePupilSize  = M(:,22);
+pa.other.focusDistance      = M(:,23);
+pa.other.focusStability     = M(:,24);
+pa.HMD.gaze_dir             = struct3Coordinate(M(:,25:27));
+pa.world.gaze_dir           = struct3Coordinate(M(:,28:30));
+pa.HMD.gaze_origin          = struct3Coordinate(M(:,31:33));
+pa.world.gaze_origin        = struct3Coordinate(M(:,34:36));
+pa.local.v                  = struct3Coordinate(M(:,37:39));
+pa.local.v_smooth           = struct3Coordinate(M(:,40:42));
+pa.world.v                  = struct3Coordinate(M(:,43:45));
+pa.world.v_smooth           = struct3Coordinate(M(:,46:48));
+pa.world.rb_v               = struct3Coordinate(M(:,49:51));
+pa.local.rb_v               = struct3Coordinate(M(:,52:54));
 
-pa.distance = M(:,12);
-pa.other.frame = M(:,13);
-pa.other.captureTime = M(:,14);
-pa.other.hmdpos = struct3Coordinate(M(:,15:17));
-pa.other.hmdrot = struct3Coordinate(M(:,18:20));
-pa.other.leftEyePupilSize = M(:,21);
-pa.other.rightEyePupilSize = M(:,22);
-pa.other.focusDistance = M(:,23);
-pa.other.focusStability = M(:,24);
 
-pa.HMD.gaze_dir = struct3Coordinate(M(:,25:27));
-pa.world.gaze_dir = struct3Coordinate(M(:,28:30));
-pa.HMD.gaze_origin = struct3Coordinate(M(:,31:33));
-pa.world.gaze_origin = struct3Coordinate(M(:,34:36));
-
-pa.local.v = struct3Coordinate(M(:,37:39));
-pa.local.v_smooth = struct3Coordinate(M(:,40:42));
-pa.world.v = struct3Coordinate(M(:,43:45));
-pa.world.v_smooth = struct3Coordinate(M(:,46:48));
-pa.world.rb_v = struct3Coordinate(M(:,49:51));
-pa.local.rb_v = struct3Coordinate(M(:,52:54));
-
-%% Data distraction AV if present
-%Need to write logic to determine whether AV is present
 if(extracar == true)
-%Data.diAV = M(:,53:96);
-diAV.pos = struct3Coordinate(M(:,53:55));
-diAV.distance = M(:,60);
-diAV.world.gaze_dir = M(:,78);
+    % Data distraction AV if present (55:100)
+    diAV.pos                        = struct3Coordinate(M(:,55:57));
+    diAV.rot                        = struct3Coordinate(M(:,58:60));
+    diAV.blinkers                   = M(:,61);
+    diAV.press                      = M(:,62);
+    diAV.release                    = M(:,63);
+    diAV.distance                   = M(:,64);
+    diAV.other.frame                = M(:,65);
+    diAV.other.captureTime          = M(:,66);
+    diAV.other.hmdpos               = struct3Coordinate(M(:,67:69));
+    diAV.other.hmdrot               = struct3Coordinate(M(:,70:72));
+    diAV.other.leftEyePupilSize     = M(:,73);
+    diAV.other.rightEyePupilSize    = M(:,74);
+    diAV.other.focusDistance        = M(:,75);
+    diAV.other.focusStability       = M(:,76);
+    diAV.HMD.gaze_dir               = struct3Coordinate(M(:,77:79));
+    diAV.world.gaze_dir             = struct3Coordinate(M(:,80:82));
+    diAV.HMD.gaze_origin            = struct3Coordinate(M(:,83:85));
+    diAV.world.gaze_origin          = struct3Coordinate(M(:,86:88));
+    diAV.local.v                    = struct3Coordinate(M(:,89:91));
+    diAV.local.v_smooth             = struct3Coordinate(M(:,92:94));
+    diAV.world.v                    = struct3Coordinate(M(:,95:97));
+    diAV.world.v_smooth             = struct3Coordinate(M(:,98:100));
 
-
-%% Data pedestrian (when AV is present)
-pe.pos = struct3Coordinate(M(:,97:99));
-pe.rot = struct3Coordinate(M(:,100:102));
-
-pe.distance = M(:,103);
-pe.distance2 = M(:,144);
-pe.distance3 = M(:,177);
-
-pe.other.frame = M(:,104);
-pe.other.captureTime = M(:,105);
-pe.other.hmdpos = struct3Coordinate(M(:,106:108));
-pe.other.hmdrot = struct3Coordinate(M(:,109:111));
-pe.other.leftEyePupilSize = M(:,112);
-pe.other.rightEyePupilSize = M(:,113);
-pe.other.focusDistance = M(:,114);
-pe.other.focusStability = M(:,115);
-
-pe.HMD.gaze_dir = struct3Coordinate(M(:,116:118));
-pe.world.gaze_dir = struct3Coordinate(M(:,119:121));
-pe.HMD.gaze_origin = struct3Coordinate(M(:,122:124));
-pe.world.gaze_origin = struct3Coordinate(M(:,125:127));
-
-pe.gapAcceptance = M(:,128);
+    % Data pedestrian when AV is present (101:132)
+    pe.pos                      = struct3Coordinate(M(:,101:103));
+    pe.rot                      = struct3Coordinate(M(:,104:106));
+    pe.distance                 = M(:,107);
+    pe.other.frame              = M(:,108);
+    pe.other.captureTime        = M(:,109);
+    pe.other.hmdpos             = struct3Coordinate(M(:,110:112));
+    pe.other.hmdrot             = struct3Coordinate(M(:,113:115));
+    pe.other.leftEyePupilSize   = M(:,116);
+    pe.other.rightEyePupilSize  = M(:,117);
+    pe.other.focusDistance      = M(:,118);
+    pe.other.focusStability     = M(:,119);
+    pe.HMD.gaze_dir             = struct3Coordinate(M(:,120:122));
+    pe.world.gaze_dir           = struct3Coordinate(M(:,123:125));
+    pe.HMD.gaze_origin          = struct3Coordinate(M(:,126:128));
+    pe.world.gaze_origin        = struct3Coordinate(M(:,129:131));
+    pe.gapAcceptance            = M(:,132);
 end
 
-%% Data pedestrian (when AV is absent)
 if(extracar == false)
+    % Data pedestrian when AV is absen (55:86)
     diAV = 0;
-pe.pos = struct3Coordinate(M(:,55:57));
-pe.rot = struct3Coordinate(M(:,58:60));
-
-pe.distance = M(:,61);
-
-pe.other.frame = M(:,62);
-pe.other.captureTime = M(:,63);
-pe.other.hmdpos = struct3Coordinate(M(:,64:66));
-pe.other.hmdrot = struct3Coordinate(M(:,67:69));
-pe.other.leftEyePupilSize = M(:,70);
-pe.other.rightEyePupilSize = M(:,71);
-pe.other.focusDistance = M(:,72);
-pe.other.focusStability = M(:,73);
-
-pe.HMD.gaze_dir = struct3Coordinate(M(:,74:76));
-pe.world.gaze_dir = struct3Coordinate(M(:,77:79));
-pe.HMD.gaze_origin = struct3Coordinate(M(:,80:82));
-pe.world.gaze_origin = struct3Coordinate(M(:,83:85));
-
-pe.gapAcceptance = M(:,86);
+    
+    pe.pos                      = struct3Coordinate(M(:,55:57));
+    pe.rot                      = struct3Coordinate(M(:,58:60));
+    pe.distance                 = M(:,61);
+    pe.other.frame              = M(:,62);
+    pe.other.captureTime        = M(:,63);
+    pe.other.hmdpos             = struct3Coordinate(M(:,64:66));
+    pe.other.hmdrot             = struct3Coordinate(M(:,67:69));
+    pe.other.leftEyePupilSize   = M(:,70);
+    pe.other.rightEyePupilSize  = M(:,71);
+    pe.other.focusDistance      = M(:,72);
+    pe.other.focusStability 	= M(:,73);
+    pe.HMD.gaze_dir             = struct3Coordinate(M(:,74:76));
+    pe.world.gaze_dir           = struct3Coordinate(M(:,77:79));
+    pe.HMD.gaze_origin          = struct3Coordinate(M(:,80:82));
+    pe.world.gaze_origin        = struct3Coordinate(M(:,83:85));
+    pe.gapAcceptance            = M(:,86);
 end
-%%
+%
 t = toc;
-
 disp(['Loading data took ', num2str(t) ,' seconds.']);
 return
