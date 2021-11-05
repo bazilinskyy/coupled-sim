05fbef17eba2437088b2ec94ff172bf0c9d0b92a
mjoaris <57585068+mjoaris@users.noreply.github.com>
2021-11-05 10:44:04 +0100

    Add HUD Position setter script
    The head up display moves and rotates with head movements of the participant, and stay at a fixed relative distance to the participants eyes.

---
+++ Assets/Scripts/New/HeadLockedPositionSetter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadLockedPositionSetter : MonoBehaviour
+{
+    // Instantiate parameters
+    private Vector3 pedPos;
+    private Quaternion pedRot;
+    private Vector3 iniViewPosDisplacement;
+    private Vector3 currentViewPosDisplacement;
+    private Vector3 iniRotAngles;
+    private Quaternion iniViewRotDisplacement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set Initial View Position and Rotation Displacement
+        iniViewPosDisplacement = new Vector3(-0.1f, 1.7f, 0.6f);
+        iniRotAngles = new Vector3(-5f, -12f, 0f);
+        iniViewRotDisplacement = Quaternion.Euler(iniRotAngles);
+
+        // Find initial Pedestrian position and rotations
+        pedPos = GameObject.Find("Participant(Clone)").transform.position;
+        pedRot = GameObject.Find("Participant(Clone)").transform.rotation;
+
+        // Set initial position and rotation
+        this.GetComponent<RectTransform>().position = pedPos + iniViewPosDisplacement;
+        this.GetComponent<RectTransform>().rotation = pedRot * iniViewRotDisplacement;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Grab current pedestrian rotations and positions:
+        pedPos = GameObject.Find("Participant(Clone)").transform.position;
+        pedRot = GameObject.Find("Participant(Clone)").transform.rotation;
+
+        // Sets the new rotation based on head movement
+        this.GetComponent<RectTransform>().rotation = pedRot * iniViewRotDisplacement;
+
+        // Rotate the displacement vector to point to new gaze direction
+        currentViewPosDisplacement = pedRot * iniViewPosDisplacement; 
+
+        // Sets the new position of the head locked display
+        this.GetComponent<RectTransform>().position = pedPos + currentViewPosDisplacement;        
+    }
+}
