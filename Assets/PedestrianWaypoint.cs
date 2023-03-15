cede6019d7f21d508bbfdcd663325f9ae49f4b06
Piotr Sienkowski <piotr@lowpolydreams.com>
2023-03-15 17:14:31 +0100

    pedestrian lights and new pedestrian controlling logic

---
+++ Assets/PedestrianWaypoint.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedestrianWaypoint : MonoBehaviour
+{
+    public AIPedestrian target;
+    public float targetSpeed = 1.6f;
+}
--- Assets/Scripts/AIPedestrians/AIPedestrian.cs
+++ Assets/Scripts/AIPedestrians/AIPedestrian.cs
@@ -5,14 +5,26 @@ public class AIPedestrian : MonoBehaviour
 {
     WaypointProgressTracker _tracker;
     public float moveSpeed = 1;
-    public float dampingFactor = 0.1f;
-
+    float currentSpeed;
+    public float HeightDampingFactor = 0.05f;
+    public float SpeedDampingFactor = 0.05f;
+    public float referenceSpeed = 1.6f;
+    public Animator animator;
     public void Init(WaypointCircuit circuit)
     {
         enabled = true;
         _tracker = GetComponent<WaypointProgressTracker>();
         _tracker.enabled = true;
         _tracker.Init(circuit);
+        foreach (var waypoint in circuit.Waypoints)
+        {
+            var speedSettings = waypoint.GetComponent<PedestrianWaypoint>();
+            if (speedSettings != null)
+            {
+                speedSettings.target = this;
+            }
+        }
+        currentSpeed = moveSpeed;
     }
 
     // Smoothing rate dictates the proportion of source remaining after one second
@@ -23,16 +35,28 @@ public class AIPedestrian : MonoBehaviour
 
     private void Update()
     {
+        currentSpeed = Damp(currentSpeed, moveSpeed, SpeedDampingFactor, Time.deltaTime);
+        animator.SetFloat("Speed", currentSpeed / referenceSpeed);
         var steer = Quaternion.LookRotation(_tracker.target.position - transform.position, Vector3.up).eulerAngles;
         var rot = transform.eulerAngles;
         rot.y = steer.y;
         transform.eulerAngles = rot;
         var pos = transform.position;
-        pos += transform.forward * moveSpeed * Time.deltaTime;
+        pos += transform.forward * currentSpeed * Time.deltaTime;
         if (Physics.Raycast(pos + Vector3.up, Vector3.down, out RaycastHit hitInfo, 2))
         {
-            pos.y = Damp(pos.y, hitInfo.point.y, dampingFactor, Time.deltaTime);
+            pos.y = Damp(pos.y, hitInfo.point.y, HeightDampingFactor, Time.deltaTime);
         }
         transform.position = pos;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        PedestrianWaypoint speedSettings = other.GetComponent<PedestrianWaypoint>();
+        if (speedSettings == null || (speedSettings.target != null && speedSettings.target != this))
+        {
+            return;
+        }
+        moveSpeed = speedSettings.targetSpeed;
     }
 }
