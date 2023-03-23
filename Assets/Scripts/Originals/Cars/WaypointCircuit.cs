7a5232a984aa5a68b1c47180427231035cd768e9
Piotr Sienkowski <piotr@lowpolydreams.com>
2023-03-23 17:30:11 +0100

    simplified waypoint circiut editor to get it into properly working state

--- Assets/Scripts/Originals/Cars/WaypointCircuit.cs
+++ Assets/Scripts/Originals/Cars/WaypointCircuit.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.IO;
 #if UNITY_EDITOR
 using UnityEditor;
-
 #endif
 
 namespace UnityStandardAssets.Utility
@@ -206,170 +205,25 @@ namespace UnityStandardAssets.Utility
     }
 }
 
+#if UNITY_EDITOR
 namespace UnityStandardAssets.Utility.Inspector
 {
-#if UNITY_EDITOR
-    [CustomPropertyDrawer(typeof(WaypointCircuit.WaypointList))]
-    public class WaypointListDrawer : PropertyDrawer
+    [CustomEditor(typeof(WaypointCircuit))]
+    public class WaypointCircuitEditor : Editor
     {
-        private float lineHeight = 18;
-        private float spacing = 4;
-
-        public object FileDialog { get; private set; }
-
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        public override void OnInspectorGUI()
         {
-            EditorGUI.BeginProperty(position, label, property);
-
-            float x = position.x;
-            float y = position.y;
-            float inspectorWidth = position.width;
-
-            // Draw label
-
-
-            // Don't make child fields be indented
-            var indent = EditorGUI.indentLevel;
-            EditorGUI.indentLevel = 0;
-
-
-            var items = property.FindPropertyRelative("items");
-            var titles = new string[] { "Transform", "", "", "" };
-            var props = new string[] { "transform", "^", "v", "-" };
-            var widths = new float[] { .7f, .1f, .1f, .1f };
-            float lineHeight = 18;
-            bool changedLength = false;
-            if (items.arraySize > 0)
-            {
-                for (int i = 0; i < items.arraySize; ++i)
-                {
-                    var item = items.GetArrayElementAtIndex(i);
-
-                    float rowX = x;
-                    for (int n = 0; n < props.Length; ++n)
-                    {
-                        float w = widths[n] * inspectorWidth;
-
-                        // Calculate rects
-                        Rect rect = new Rect(rowX, y, w, lineHeight);
-                        rowX += w;
-
-                        if (i == -1)
-                        {
-                            EditorGUI.LabelField(rect, titles[n]);
-                        }
-                        else
-                        {
-                            if (n == 0)
-                            {
-                                EditorGUI.ObjectField(rect, item.objectReferenceValue, typeof(Transform), true);
-                            }
-                            else
-                            {
-                                if (GUI.Button(rect, props[n]))
-                                {
-                                    switch (props[n])
-                                    {
-                                        case "-":
-                                            items.DeleteArrayElementAtIndex(i);
-                                            items.DeleteArrayElementAtIndex(i);
-                                            changedLength = true;
-                                            break;
-                                        case "v":
-                                            if (i > 0)
-                                            {
-                                                items.MoveArrayElement(i, i + 1);
-                                            }
-                                            break;
-                                        case "^":
-                                            if (i < items.arraySize - 1)
-                                            {
-                                                items.MoveArrayElement(i, i - 1);
-                                            }
-                                            break;
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                    y += lineHeight + spacing;
-                    if (changedLength)
-                    {
-                        break;
-                    }
-                }
-            }
-            // add button
-            var addButtonRect = new Rect((x + position.width) - widths[widths.Length - 1] * inspectorWidth, y,
-                                         widths[widths.Length - 1] * inspectorWidth, lineHeight);
-            if (GUI.Button(addButtonRect, "+"))
-            {
-                items.InsertArrayElementAtIndex(items.arraySize);
-            }
-
-            y += lineHeight + spacing;
-
-            // add all button
-            var addAllButtonRect = new Rect(x, y, inspectorWidth, lineHeight);
-            if (GUI.Button(addAllButtonRect, "Assign using all child objects"))
-            {
-                var circuit = property.FindPropertyRelative("circuit").objectReferenceValue as WaypointCircuit;
-                var children = new Transform[circuit.transform.childCount];
-                int n = 0;
-                foreach (Transform child in circuit.transform)
-                {
-                    children[n++] = child;
-                }
-                Array.Sort(children, new TransformNameComparer());
-                circuit.waypointList.items = new Transform[children.Length];
-                for (n = 0; n < children.Length; ++n)
-                {
-                    circuit.waypointList.items[n] = children[n];
-                }
-                EditorUtility.SetDirty(circuit);
-            }
-            y += lineHeight + spacing;
-
-            // rename all button
-            var renameButtonRect = new Rect(x, y, inspectorWidth, lineHeight);
-            if (GUI.Button(renameButtonRect, "Auto Rename numerically from this order"))
-            {
-                var circuit = property.FindPropertyRelative("circuit").objectReferenceValue as WaypointCircuit;
-                int n = 0;
-                foreach (Transform child in circuit.waypointList.items)
-                {
-                    child.name = "Waypoint " + (n++).ToString("000");
-                    EditorUtility.SetDirty(child);
-                }
-            }
-            y += lineHeight + spacing;
-
-            var importButtonRect = new Rect(x, y, inspectorWidth, lineHeight);
-            if (GUI.Button(importButtonRect, "Import from file"))
+            DrawDefaultInspector();
+            var waypointList = serializedObject.FindProperty("waypointList");
+            var items = waypointList.FindPropertyRelative("items");
+            if (GUILayout.Button("Import from file"))
             {
                 ImportFromFile(items.serializedObject);
             }
-            y += lineHeight + spacing;
-
-            var exportButtonRect = new Rect(x, y, inspectorWidth, lineHeight);
-            if (GUI.Button(exportButtonRect, "Export to file"))
+            if (GUILayout.Button("Export to file"))
             {
                 ExportToFile(items.serializedObject);
             }
-            y += lineHeight + spacing;
-
-            // Set indent back to what it was
-            EditorGUI.indentLevel = indent;
-            EditorGUI.EndProperty();
-        }
-
-
-        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-        {
-            SerializedProperty items = property.FindPropertyRelative("items");
-            float lineAndSpace = lineHeight + spacing;
-            return 40 + (items.arraySize*lineAndSpace) + lineAndSpace * 3;
         }
 
         void ExportToFile(SerializedObject circuitObject)
@@ -391,7 +245,8 @@ namespace UnityStandardAssets.Utility.Inspector
                 if (s != null)
                 {
                     sb.Append($";{(int)s.Type};{s.speed};{s.acceleration};{(int)s.BlinkerState};{s.jerk};{s.causeToYield};{s.EyeContactWhileYielding};{s.EyeContactAfterYielding};{s.yieldTime};{s.brakingAcceleration};{s.YieldingEyeContactSince};{s.YieldingEyeContactUntil};{s.GetCustomBehaviourDataString()}");
-                } else
+                }
+                else
                 {
                     sb.Append($";;;;;;;;;;;;;");
                 }
@@ -532,8 +387,8 @@ namespace UnityStandardAssets.Utility.Inspector
                     //Debug.LogWarning("Circuit could not be fully deserialized. Fill customBehaviourData with: " + customBehaviourDataString);
                     var ids = customBehaviourDataString.Split("%");
                     speedSettings.customBehaviourData = new CustomBehaviourData[ids.Length];
-                    for(int j = 0; j < ids.Length; j++)
-                    { 
+                    for (int j = 0; j < ids.Length; j++)
+                    {
                         var id = ids[j].Split("#")[1];
                         speedSettings.customBehaviourData[j] = EditorUtility.InstanceIDToObject(int.Parse(id)) as CustomBehaviourData;
                     }
@@ -561,7 +416,7 @@ namespace UnityStandardAssets.Utility.Inspector
 
             circuitObject.ApplyModifiedProperties();
         }
-        
+
 
         // comparer for check distances in ray cast hits
         public class TransformNameComparer : IComparer
@@ -572,5 +427,6 @@ namespace UnityStandardAssets.Utility.Inspector
             }
         }
     }
-#endif
 }
+#endif
+
