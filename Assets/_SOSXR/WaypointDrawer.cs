d98a57f60dcebfb31380cfca7085ca47fe920fc4
fwalker89 <francesco.walker89@gmail.com>
2024-01-29 16:11:58 +0100

    Basic waypoint drawer

---
+++ Assets/_SOSXR/WaypointDrawer.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Utility;
+
+[ExecuteAlways]
+[RequireComponent(typeof(WaypointCircuit))]
+public class WaypointDrawer : MonoBehaviour
+{
+   private WaypointCircuit _waypointCircuit;
+
+
+
+    private void Awake ()
+    {
+        _waypointCircuit = GetComponent<WaypointCircuit>();
+       
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < _waypointCircuit.Waypoints.Length ; i++)
+        {
+            if (i == _waypointCircuit.Waypoints.Length - 1)
+            {
+                Gizmos.DrawLine(_waypointCircuit.Waypoints[i].transform.position, _waypointCircuit.Waypoints[0].transform.position);
+            }
+            else
+            {
+                Gizmos.DrawLine(_waypointCircuit.Waypoints[i].transform.position, _waypointCircuit.Waypoints[i+1].transform.position);
+            }
+
+            Gizmos.DrawSphere(_waypointCircuit.Waypoints[i].transform.position, 0.25f);
+        }
+    }
+}
+
+
