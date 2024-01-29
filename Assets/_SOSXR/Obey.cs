59cc7f46d38e1a4f953103b98b60fd931f1b7536
fwalker89 <francesco.walker89@gmail.com>
2024-01-29 19:56:00 +0100

    Refactored Obey

--- Assets/_SOSXR/Obey.cs
+++ Assets/_SOSXR/Obey.cs
@@ -1,19 +1,18 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine;
-using System.Collections;
 using UnityEngine.Events;
-using UnityEngine.Serialization;
 
-
+/// <summary>
+/// This component needs to live on its own GameObject as a child to each Car traffic light.
+/// It asks the traffic light it's light state (red/green), and modifies it's own created SpeedSettings component.
+/// It spawns a pretty BoxCollider. The car, when inside this box, picks up the SpeedSettings setting in this BoxCollider.
+/// For this to work, the AICar needed to have their checking done in OnTriggerStay instead of OnTriggerEnter. Shouldn't matter too much.
+/// This way, we can set the Speed to 0 and the acceleration to a negative number, when light == red. When green: pedal to the metal.
+/// Disabling the obey-toggle disables this system, allowing the car to run a red light.
+/// It will then fire an event. Hook this to the audio system for instance for a half-hearted apology by the (AI-) driver.
+/// </summary>
 public class Obey : MonoBehaviour
 {
-    private CarTrafficLight m_carTrafficLight;
-    private SpeedSettings _speedSettings;
-    private BoxCollider _boxCollider;
-    private SpeedSettings[] _allSpeedSettings;
     [SerializeField] [Range(1,50)] private float m_colliderLength = 20f;
     [SerializeField] [Range(-5,-1)] private float m_deceleration = -3f;
     [SerializeField] [Range(1,5)] private float m_acceleration = 2f;
@@ -22,6 +21,12 @@ public class Obey : MonoBehaviour
 
     [SerializeField] private UnityEvent m_eventOnRunningRedLight;
 
+    private CarTrafficLight m_carTrafficLight;
+    private SpeedSettings _speedSettings;
+    private BoxCollider _boxCollider;
+    private SpeedSettings[] _allSpeedSettings;
+    
+    
     private void Awake()
     {
         gameObject.tag = "WP";
@@ -61,7 +66,8 @@ public class Obey : MonoBehaviour
             if (theirBox.bounds.Intersects(_boxCollider.bounds))
             {
                 theirBox.gameObject.SetActive(false);
-                Debug.Log("One other SpeedSetting component was in my way, I disabled their GameObject completely.");
+                Debug.LogWarning("One other SpeedSetting component was in my way, I disabled their GameObject completely.");
+                // This needed doing because otherwise that other speedsetter would interfere, and make the car accelerate for instance when it should stop for red light
             }
         }
     }
@@ -71,7 +77,6 @@ public class Obey : MonoBehaviour
     {
         if (!m_obey)
         {
-            Debug.Log("We're not listening to this traffic light here");
             return;
         }
         
@@ -79,17 +84,36 @@ public class Obey : MonoBehaviour
         {
             _speedSettings.speed = 0f;
             _speedSettings.acceleration = m_deceleration;
-            Debug.Log("Boem is ho");
         }
         else if (m_carTrafficLight.State == LightState.GREEN)
         {
             _speedSettings.speed = m_speedWhenGoing;
             _speedSettings.acceleration = m_acceleration;
-            Debug.Log("Green is go");
         }
-        else 
+        else
         {
-             Debug.Log("These light colours are not yet implemented");
+            // This is where you might wanna put other states as well. 
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var aiCar = other.GetComponent<AICar>();
+
+        if (aiCar == null)
+        {
+            return;
+        }
+
+        if (!aiCar.isActiveAndEnabled)
+        {
+            return;
+        }
+
+        if (!m_obey)
+        {
+            Debug.LogFormat("We're not listening to this traffic light here! We're bad bad drivers. The light is now {0}", m_carTrafficLight.State.ToString());
         }
     }
 
@@ -113,12 +137,14 @@ public class Obey : MonoBehaviour
             return;
         }
 
-        if (m_carTrafficLight.State != LightState.RED)
+        if (m_carTrafficLight.State == LightState.RED)
         {
-            return;
+            m_eventOnRunningRedLight?.Invoke();
+            Debug.Log("We ran a red light and now are firing this UnityEvent. Pick this up by any other component. E.g.: this is where you'd hook up the audio system ('SORRY NOT SORRY!') for instance");
         }
-        
-        m_eventOnRunningRedLight?.Invoke();
-        Debug.Log("We ran a red light and now are firing this event. This is where you'd hook up the audio events for instance");
+        else 
+        {
+            Debug.Log("This could also be a place to add multiple functions for other light states. Like what to do when light was Yellow? Is not a traffic violation, but might be something interesting anyway.");
+        }
     }
 }
