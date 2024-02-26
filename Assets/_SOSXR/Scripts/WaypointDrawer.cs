f7587c94ccbfcd8a2ca3aca4a2a1bb93e14e694d
fwalker89 <francesco.walker89@gmail.com>
2024-02-26 18:48:20 +0100

    Updated waypointdrawer to make loop to start optional. Plus colours

--- Assets/_SOSXR/Scripts/WaypointDrawer.cs
+++ Assets/_SOSXR/Scripts/WaypointDrawer.cs
@@ -6,6 +6,7 @@ using UnityStandardAssets.Utility;
 [RequireComponent(typeof(WaypointCircuit))]
 public class WaypointDrawer : MonoBehaviour
 {
+    [SerializeField] private bool m_loopToStart;
     private WaypointCircuit _waypointCircuit;
 
 
@@ -19,23 +20,32 @@ public class WaypointDrawer : MonoBehaviour
     {
         for (var i = 0; i < _waypointCircuit.Waypoints.Length; i++)
         {
-            
             if (i == _waypointCircuit.Waypoints.Length - 1)
             {
-                Gizmos.DrawLine(_waypointCircuit.Waypoints[i].transform.position, _waypointCircuit.Waypoints[0].transform.position);
+                if (m_loopToStart)
+                {
+                    Gizmos.color = Color.blue;
+                    Gizmos.DrawLine(_waypointCircuit.Waypoints[i].transform.position, _waypointCircuit.Waypoints[0].transform.position);
+                }
+
+                Gizmos.color = Color.red;
+                Gizmos.DrawCube(_waypointCircuit.Waypoints[i].transform.position, Vector3.one * 2);
             }
             else
             {
+                Gizmos.color = Color.blue;
                 Gizmos.DrawLine(_waypointCircuit.Waypoints[i].transform.position, _waypointCircuit.Waypoints[i + 1].transform.position);
             }
-            
+
 
             if (i == 0)
             {
-                Gizmos.DrawCube(_waypointCircuit.Waypoints[i].transform.position, Vector3.one);
+                Gizmos.color = Color.green;
+                Gizmos.DrawCube(_waypointCircuit.Waypoints[i].transform.position, Vector3.one * 2);
             }
             else
             {
+                Gizmos.color = Color.yellow;
                 Gizmos.DrawSphere(_waypointCircuit.Waypoints[i].transform.position, 0.25f);
             }
         }
